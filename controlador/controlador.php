380bdecd7aaa9b97e79114133e21ca4c2212b45d
Jesusdlsc <126646041+Jesusdlsc@users.noreply.github.com>
2023-03-25 20:26:33 -0500

    Add files via upload

---
+++ controlador/controlador.php
@@ -0,0 +1,53 @@
+<?php
+
+require 'DB/conexion_bd.php';
+
+class musica extends BD_PDO
+{
+	function Insertar($imagen, $cantante,$ncancion,$cancion)
+	{
+		$this->Ejecutar_Instruccion("Insert into musica(imagen,cantante,ncancion,cancion) values('$imagen','$cantante','$ncancion','$cancion')");
+	}
+
+	function Buscar($buscar)
+	{
+		$result = $this->Ejecutar_Instruccion("Select * from musica where cantante like '%$buscar%'");
+		return $result;
+	}
+
+	function Buscar_todo()
+	{
+		$result = $this->Ejecutar_Instruccion("Select * from musica");
+		return $result;
+	}
+
+    function ideliminar($ideliminar)
+    {
+		$this -> Ejecutar_Instruccion("Delete from musica where id_cantante = '$ideliminar'");
+	}
+
+	function idmodificar($idmodificar)
+    {
+		$result= $this->Ejecutar_Instruccion("Select * from musica where id_cantante = '$idmodificar'");
+		return $result;
+	}
+
+	function Tabla_gen($result)
+	{
+		$tabla="";
+		foreach ($result as $renglon) 
+		{
+			$tabla.="<tr>";
+			$tabla.='<td align="center">'.$renglon[0].'</td>';
+			$tabla.='<td align="center"><img src='.$renglon[1].' width="100" height="100"></td>';
+			$tabla.='<td align="center">'.$renglon[2].'</td>';
+			$tabla.='<td align="center">'.$renglon[3].'</td>';
+			$tabla.='<td align="center"><audio controls><source src='.$renglon[4].' type="audio/mpeg"</td>';
+			$tabla.='<td align="center"><input type="button" id="btneliminar" name="btneliminar" value="Eliminar" onclick="javascript: eliminar('.$renglon[0].');"></td>';
+			$tabla.='<td align="center"><input type="button" id="btnmodificar" name="btnmodificar" value="Modificar" onclick="javascript: modificar('.$renglon[0].');"></td>
+				</tr>';
+		}
+		return $tabla;
+	}
+}
+?>
---
+++ vista/vistadm.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<title>Download DC</title>
+	<meta content="" name="description" />
+	<meta content="" name="keywords" />
+
+	<!--Vendor CSS FILES -->
+	<link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet" />
+  <link href="assets/css/inadm.css" rel="stylesheet">
+
+<script>
+	
+	function eliminar(id)
+	{
+		if(confirm("¿ Estas seguro de eliminar el registro ?"))
+		{
+			window.location = "indexadm.php?ideliminar=" + id;
+		}
+	}
+
+	function modificar(id)
+	{
+		window.location = "indexadm.php?idmodificar=" + id;
+	}
+</script>
+</head>
+<header id="header" class="header d-flex align-items-center fixed-top">
+      <div
+        class="container-fluid d-flex align-items-center justify-content-between"
+      >
+        <a
+          href="index.html"
+          class="logo d-flex align-items-center me-auto me-lg-0"
+        >
+          <!-- Uncomment the line below if you also wish to use an image logo -->
+          <!-- <img src="assets/img/logo.png" alt=""> -->
+          <i class="bi bi-music-note-list"></i>
+          <h1>Download DC</h1>
+        </a>
+
+        <!-- Inicio de sesion -->
+        <div class="header-social-links">
+        <a href="gallery-single.html" data-bs-toggle="modal" data-bs-target="#RegistrarModal" class="details-link">Agregar cantante</a>        <!-- Button trigger modal -->
+          <a style="margin: 0px" href="./index.php"
+            ><img
+              width="40px"
+              style="filter: invert(100%); margin: auto 20px;"
+              src="./assets/img/left flecha.png"
+              alt=""
+          /></a>
+        </div>
+        <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
+        <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
+      </div>
+    </header>
+  <!-- Modal Regisrar usuarios -->
+  <div class="modal fade" id="RegistrarModal" style=" background-color: rgba(0, 0, 0, 0.5) !important;" tabindex="1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content" style="  color: #fff !important;">
+      <div class="modal-header">
+        <img id="imagen_modal" src="./assets/img/logo integradora.png" alt="">
+        <!-- <h2 class="modal-title " id="exampleModalLabel" style="color: #000;">Download DC</h2> -->
+        <!-- <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> -->
+      </div>
+      <div class="modal-body modal_inicio" style="color: #fff;">
+                <!-- Inicia Fomulario -->
+        <form method="post">
+        <h1>Registro</h1>
+		<form action="indexadm.php" id="frminsertar" name="frminsertar" method="post">
+		
+		<br>
+		<label for="">Cantante: </label>
+		<input type="text" id="txtnombre" name="cantante" value="<?php echo @$buscar_mod[0][1]; ?>" style="width: 50%;
+		border: none; outline: none; background: #e6e6fa; border-bottom: 3px solid">
+		<br>
+		<br>
+		<label for="">Direccion imagen: </label>
+		<input type="text" id="txtfecha" name="imagen" value="<?php echo @$buscar_mod[0][2]; ?>" style="width: 50%;
+		border: none; outline: none; background: #e6e6fa; border-bottom: 3px solid">
+		<br>
+		<br>
+		<label for="">Nombre cancion: </label>
+		<input type="text" id="txtfecha" name="ncancion" value="<?php echo @$buscar_mod[0][3]; ?>" style="width: 50%;
+		border: none; outline: none; background: #e6e6fa; border-bottom: 3px solid">
+		<br>
+		<br>
+		<label for="">Direccion cancion: </label>
+		<input type="text" id="txtfecha" name="cancion" value="<?php echo @$buscar_mod[0][4]; ?>" style="width: 50%;
+		border: none; outline: none; background: #e6e6fa; border-bottom: 3px solid">
+		<br>
+		<br>
+		<input type="submit" id="btninsertar" name="btninsertar" value="<?php if(isset($_GET['idmodificar']))
+		{ 
+			echo 'Modificar';
+		}
+		else
+		{
+			echo 'Insertar';
+		}
+		 ?>">
+		 </center>
+	</form>
+	</div>
+        <div class="modal-footer modal_inicio">
+          <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Cancelar</button>
+        </div>
+      </form>      <!-- Termina Forulario -->
+    </div>
+  </div>
+</div> <!-- Cierra modal -->
+
+
+<body>	
+<div class="zona1">
+	<form action="indexadm.php" id="frmbuscar" name="frmbuscar" method="post">
+		<label for="">Buscar: </label>
+		<input type="text" id="txtbuscar" name="txtbuscar" style="width: 11%;
+		border: none; outline: none; background: #e6e6fa; border-bottom: 1px solid">
+		<input type="submit" id="btnbuscar" name="btnbuscar" value="Buscar" >
+		<br><br>
+		<table class= "table table-striped">
+			<tr>
+				<td align="center" height="50" width="150"  >Número</td>
+				<td align="center"  height="50" width="150" >Imagen</td>
+				<td align="center" height="50" width="150"  >Cantante</td>
+				<td align="center" height="50" width="150"  >Nombre cancion</td>
+				<td align="center" height="50" width="150"  >Cancion</td>
+
+				<td bgcolor="00FF00" style="border-right: 1px solid;" colspan="2" align="center">Accion</td>
+			</tr>
+			<?php echo $datos; ?>
+		</table>
+	</form>
+	</center>
+	</div>
+	<div id="preloader">
+      <div class="line"></div>
+    </div>
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+	
+</body>
+</html>
